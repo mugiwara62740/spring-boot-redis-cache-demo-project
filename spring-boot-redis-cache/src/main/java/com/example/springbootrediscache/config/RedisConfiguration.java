95a09db409ebd224a6fef3c9247153dacfc4e82b
Elouahmani <os-younes.elouahmani@rakuten.com>
2020-11-18 10:00:39 +0100

    Test 2 - master slave architecture

--- spring-boot-redis-cache/src/main/java/com/example/springbootrediscache/config/RedisConfiguration.java
+++ spring-boot-redis-cache/src/main/java/com/example/springbootrediscache/config/RedisConfiguration.java
@@ -1,19 +1,13 @@
-
-/*package com.example.springbootrediscache.config;
+package com.example.springbootrediscache.config;
 
 import io.lettuce.core.ReadFrom;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Configuration
@@ -40,23 +34,11 @@ public class RedisConfiguration {
     }
 
     @Bean
-    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        //To use increment method in repository
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-    @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
                 .readFrom(ReadFrom.REPLICA_PREFERRED)
                 .build();
         RedisStaticMasterReplicaConfiguration staticMasterReplicaConfiguration = new RedisStaticMasterReplicaConfiguration(this.getMaster().getHost(), this.getMaster().getPort());
-        staticMasterReplicaConfiguration.setPassword(RedisPassword.of("52wJmtmEVuAsL"));
         this.getSlaves().forEach(slave -> staticMasterReplicaConfiguration.addNode(slave.getHost(), slave.getPort()));
         return new LettuceConnectionFactory(staticMasterReplicaConfiguration, clientConfig);
     }
@@ -84,5 +66,3 @@ public class RedisConfiguration {
     }
 
 }
-
- */
